82ed9e9bea53dbebb5bb79d33814e9a23174a0d5
darwinledesma <dledesma@aventi.co>
2022-02-14 14:34:45 -0500

    first commit

---
+++ Hola Mundo/Hola Mundo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hola_Mundo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
