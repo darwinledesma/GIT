76e424250de39cb652b292e17d27b836d2cb9159
SANTIAGO LOPERA <slopera@aventi.co>
2022-02-15 09:43:30 -0500

    SantiagoControler  desde rama santiagoDevelop

---
+++ Hola Mundo/Hola Mundo/Controllers/SantiagoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hola_Mundo.Controllers
+{
+    public class SantiagoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
